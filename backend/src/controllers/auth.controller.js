91e829a9f31661ce89b4fd47cb6319860c50cfee
Sai Ashrith <kamatamsaiashrith@gmail.com>
2025-12-02 12:11:03 +0530

    Replace bcrypt with bcryptjs

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 // src/controllers/auth.controller.js
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const User = require('../models/User');
