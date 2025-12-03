da51de2e621e9ba7168d5ccbb2a93b2842d31616
Sai Ashrith <kamatamsaiashrith@gmail.com>
2025-12-03 10:38:38 +0530

    Fix controller import path

--- backend/src/routes/products.routes.js
+++ backend/src/routes/products.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const productCtrl = require("../controllers/product.controller");
+const productCtrl = require("../controllers/products.controller");
 const auth = require("../middleware/auth");
 
 // FARMER
